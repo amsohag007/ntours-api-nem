b4c54cdd32e188679e95f0f02357ef5be4cda247
amsohag007 <amsohag007@gmail.com>
2022-10-29 00:22:22 +0600

    testing commit issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.statusCode(200).send('Hello from server');
+  res.status(200).send('Hello from server');
 });
 
 app.listen(3000, () => {
