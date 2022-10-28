fd87dfdd8f6f98467c3aeadca421d741d850b8e9
amsohag007 <amsohag007@gamil.com>
2022-10-28 23:50:29 +0600

    basic express server setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.statusCode(200).send('Hello from server');
+});
+
+app.listen(3000, () => {
+  console.log('App is running on port 3000');
+});
